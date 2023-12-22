75a6745b0f1f51781ba15f483114f552981f6496
Alf-caput <alfre.roble.aba@gmail.com>
2023-12-22 23:31:09 +0100

    mejoras, avances en dfs

--- playground.m
+++ playground.m
@@ -6,10 +6,10 @@ A = [
     1, 0, 1, 0, 0;
 ];
 
-G = digraph(A);
+G = graph(A);
 plot(G)
+arista = [3, 4];
 
-arista = [2, 3];
 res = perteneceCiclo(A, arista);
 
 disp(res);
@@ -21,49 +21,44 @@ function res = perteneceCiclo(A, arista)
     % La matriz de adyacencia debe ser cuadrada y simétrica
     if ~(fil == col && isequal(A, A'))
         disp('Error: Matriz de adyacencia no válida');
+        res = false;
         return
     end
 
     % La arista debe tener dos componentes enteras distintas que toman valores entre 1 y el número de nodos
-    if ~(length(arista) == 2 && isinteger(arista) && arista(1) ~= arista(2) && all(v >= 1 & v <= fil))
+    if ~(length(arista) == 2 && all(arista == round(arista)) && arista(1) ~= arista(2) && all(arista >= 1 & arista <= fil))
         disp('Error: Arista no válida');
+        res = false;
         return
     end
     
-    % Eliminar arista
+    % Se elimina la arista que queremos comprobar
     A(arista, flip(arista)) = 0;
     
-    % Una arista pertenece a un ciclo si se puede ir de uno de sus extremos al otro tras eliminarla del grafo inicial 
-    res = dfs(A, arista(1), arista(2));
+    % Se recorrerá el grafo empezando por el nodo determinado por la primera componente de la arista
+    vistos = zeros(1, fil);
+    vistos(arista(1)) = 1;
+
+    % La arista pertenece a un ciclo si se puede ir de uno de sus extremos al otro tras eliminarla del grafo inicial 
+    res = dfs(A, arista(1), arista(2), vistos);
 end
 
-% function pertenece = perteneceCiclo(grafo, inicio, objetivo)    
-%     visto = zeros(1, size(grafo, 1));
-%     visto(inicio) = 1;
-%     pertenece = dfs(grafo, inicio, objetivo, visto);
-% end
-
-function res = dfs(grafo, actual, objetivo, visto)
+function res = dfs(grafo, actual, objetivo, vistos)
     vecinos = find(grafo(actual, :));
 
     for i = 1:length(vecinos)
-        vecino = vecinos(i);
-
-        if visto(vecino) == 0
-            visto(vecino) = -visto(actual);
-
-            if dfs(grafo, vecino, objetivo, visto)
-                res = true;
-                return;
-            end
-
-        elseif visto(vecino) == visto(actual)
-            res = true;
-            return;
+        vistos(vecinos(i)) = 1;
+        if vecinos(i) == objetivo
+            break
         end
-
+        if ismember(objetivo, vistos)
+            res = true;
+            break
+        end
+        arista = [actual, vecinos(i)];
+        grafo(arista, flip(arista)) = 0;
+        dfs(grafo, vecinos(i), objetivo, vistos)
+        res = false;
     end
-
-    res = false;
 
 end
